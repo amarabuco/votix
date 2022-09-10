73728044343f8176036efbb044c47190f735c8d2
amarabuco <adrianomal@gmail.com>
2022-09-10 17:33:30 -0300

    download proposta

--- app/app/pages/candidato.py
+++ app/app/pages/candidato.py
@@ -131,7 +131,7 @@ with col3:
 if cargo_id == 3:
     st.info("### Proposta")
     proposta = f"https://raw.githubusercontent.com/amarabuco/votix/data/app/app/data/propostas/{sigla}/2022{sigla}{candidato['id']}.pdf"
-    st.write(f"📔 <a target='_blank' href='{proposta}'> PROPOSTA</a>", unsafe_allow_html=True)
+    st.write(f"📔 <a target='_blank' href='{proposta}'> DOWNLOAD</a>", unsafe_allow_html=True)
 
 st.info("### Nuvem de palavras")
 if st.button('Gerar'):
